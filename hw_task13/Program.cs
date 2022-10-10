db8e3ad49bf045b1c5d4e45064e110727a1483b0
Andrey <ryazanov.aa@yandex.ru>
2022-10-11 01:47:08 +0300

    Initial commit

---
+++ hw_task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a >= 100)
+{
+    if (a > 999)
+    {
+        int b = 10;
+        while (a / b > 1000)
+        {
+            b = b * 10;
+        }
+        Console.WriteLine(a / b % 10);
+    }
+    else
+    {
+        Console.WriteLine(a % 10);
+    }
+    
+}
+else
+{
+    Console.WriteLine("Я сделал всё что мог, но третьей цифры не нашёл...");
+}
---
+++ hw_task15/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Понедельник - будний день");
+        break;
+    case 2:
+        Console.WriteLine("Вторник - - будний день");
+        break;
+    case 3:
+        Console.WriteLine("Среда - будний день");
+        break;
+    case 4:
+        Console.WriteLine("Четверг - будний день");
+        break;
+    case 5:
+        Console.WriteLine("Пятница - будний день");
+        break;
+    case 6:
+        Console.WriteLine("Суббота - наконец выходной день");
+        break;
+    case 7:
+        Console.WriteLine("Воскресение - выходной день");
+        break;
+    default:
+        Console.WriteLine("Такого дня недели нет");
+        break;
+}
