84afff4d56cbffdeefc1923cfe4a48fb4f8c431d
Andrey <ryazanov.aa@yandex.ru>
2022-10-11 13:33:48 +0300

    second commit

--- hw_task10/Program.cs
+++ hw_task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a >= 100 & a <= 999)
+{
+   int b =  a % 100 / 10;
+   Console.WriteLine(b);
+}
+else
+{
+   Console.WriteLine("Вы умудрились ввести неверное число");
+}
